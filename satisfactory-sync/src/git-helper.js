956107d5b9f05ec85833cccf321f80f618160e1e
f.david <f.david314@gmail.com>
2022-06-03 14:31:54 +0200

    Test

--- satisfactory-sync/src/git-helper.js
+++ satisfactory-sync/src/git-helper.js
@@ -3,22 +3,33 @@ const childProcess = require("child_process");
 module.exports = {
     pull(){
         console.log("Git: pull");
-        childProcess.execSync("git pull")
+        childProcess.execSync("git pull",
+        {
+            cwd: process.cwd()
+        })
     },
     add(){
         console.log("Git: add all");
-        childProcess.execSync("git add --all");
+        childProcess.execSync("git add --all", {
+            cwd: process.cwd()
+        });
     },
     commit(message){
         console.log("Git: commit with message: ", message);
-        childProcess.execSync(`git commit -m '${message}'`);
+        childProcess.execSync(`git commit -m '${message}'`, {
+            cwd: process.cwd()
+        });
     },
     push(){
         console.log("Git: push");
-        childProcess.execSync("git push origin master");
+        childProcess.execSync("git push origin master", {
+            cwd: process.cwd()
+        });
     },
     reset(){
         console.log("Git: reset");
-        childProcess.execSync("git add --all && git reset --hard");
+        childProcess.execSync("git add --all && git reset --hard", {
+            cwd: process.cwd()
+        });
     }
 };
